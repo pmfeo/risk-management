c86f75d25328e5fb622cc01f9c14fbceed08a871
pmfeo <pablo.feo.acosta@gmail.com>
2022-09-08 16:20:55 -0300

    Add Layout component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import "./App.scss";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
+    // form
+      // initial K
+      // tikr
+      // get actual price
+      // trade price
+      // trade direction
+      // risk
+      // radio SL % | fixed price
+      // SL
+    // submit button
+
+    // results
+      // shares to buy/sell
+      // SL $
+      // position value
+      // $ at risk
+
+    <div className="App">
+      <Layout>
+        <h1>Risk Management</h1>
+      </Layout>
+    </div>
     // TODO: splash screen
     // TODO: i18n
     // TODO: add open positions and recalculate available amount
     // TODO: add total risk over available amount
-
-    // form
-    // initial K
-    // tikr
-    // get actual price
-    // trade price
-    // trade direction
-    // risk
-    // radio SL % | fixed price
-    // SL
-    // submit button
-
-    // results
-    // shares to buy/sell
-    // SL $
-    // position value
-    // $ at risk
-
-    <div className="App">
-      <h1>Risk Management</h1>
-    </div>
   );
 }
 
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,11 @@
+
+
+interface Props {
+  children: React.ReactNode
+}
+
+function Layout({ children }: Props) {
+  return <div className="container">{children}</div>;
+}
+
+export default Layout;
