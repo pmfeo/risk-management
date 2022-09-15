d45d4d92ffbf5ad5a7fafb9f6ebebdeed9d7bdf2
pmfeo <pablo.feo.acosta@gmail.com>
2022-09-15 16:00:07 -0300

    Splash transition.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
-import Fade from "react-bootstrap/Fade";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Layout from "./components/layout/Layout";
@@ -12,59 +13,55 @@ import "./App.scss";
 
 function App(): JSX.Element {
   const [loading, setLoading] = useState(true);
-  const [fadeIn, setFadeIn] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setFadeIn(true);
       setLoading(false);
     }, 1000);
   }, [loading]);
 
-  if (loading) {
-    return (
-      <Fade in={loading}>
-        <SplashScreen />
-      </Fade>
-    );
-  } else {
-    return (
-      <Fade
-        in={fadeIn}
-        // timeout={900}
-        // appear
-      >
-        <div className="App">
-          <Layout>
-            <Row>
-              <Col>
-                <h1 className="mb-3 mt-3 text-center">Risk Management</h1>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h2>Enter your trade info below</h2>
-                <CalculateRiskForm />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <CalculateRiskResults />
-              </Col>
-            </Row>
-          </Layout>
-        </div>
-      </Fade>
-    );
-  }
+  // useEffect(() => {
+  //   console.log(first);
+  // });
 
-  // TODO: splash screen
-  // TODO: results animation
-  // TODO: get quote from API
-  // TODO: a11y
-  // TODO: i18n
-  // TODO: add open positions and recalculate available amount
-  // TODO: add total risk over available amount
+  return (
+    <TransitionGroup>
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <CSSTransition timeout={250} classNames="item">
+          <div className="App">
+            <Layout>
+              <Row>
+                <Col>
+                  <h1 className="mb-3 mt-3 text-center">Risk Management</h1>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h2>Enter your trade info below</h2>
+                  <CalculateRiskForm />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <CalculateRiskResults />
+                </Col>
+              </Row>
+            </Layout>
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
+  );
 }
+
+// TODO: splash screen
+// TODO: results animation
+// TODO: get quote from API
+// TODO: a11y
+// TODO: i18n
+// TODO: add open positions and recalculate available amount
+// TODO: add total risk over available amount
 
 export default App;
